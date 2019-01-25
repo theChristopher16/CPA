8ba3e0fe06b4b41c015dff41c5c34eebd781e5a4
Christopher Smith <cms095@latech.edu>
2019-01-24 21:23:42 -0600

    Started work on getting the map to work. p5.js works but is being rendered below the component. We will also need to get WEBGL working to render the 3d objects.

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as p5 from '../../../node_modules/p5/';
 
 @Component({
   selector: 'app-map',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
-  constructor() { }
+  private p5;
+
+  constructor(
+  //  private p5: p5 
+) { }
 
   ngOnInit() {
+
+    this.createCanvas();
   }
 
+  private createCanvas(){
+    this.p5 = new p5(this.sketch);
+  }
+
+  private sketch(p: any){
+    p.setup = () => {
+      p.createCanvas(window.innerWidth, window.innerHeight);
+    };
+
+    p.draw = () => {
+      p.background(255);
+      p.fill(0);
+      p.rect(p.width / 2, p.height / 2, 50, 50);
+    };
+  }
 }
