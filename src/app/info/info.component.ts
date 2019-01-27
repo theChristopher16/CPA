e01b865ae6433fca0dee315d42471504b9f74b83
Christopher Smith <christophersmithm16@gmail.com>
2019-01-27 10:36:36 -0600

    Merge fix... again.

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {SlideshowModule} from 'ng-simple-slideshow';
 
 @Component({
   selector: 'app-info',
@@ -7,9 +8,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InfoComponent implements OnInit {
 
+  imageUrls: (string | IImage)[] = [
+    { url: 'https://cdn.vox-cdn.com/uploads/chorus_image/image/56748793/dbohn_170625_1801_0018.0.0.jpg', caption: 'The first slide', href: '#config' },
+    { url: 'https://cdn.vox-cdn.com/uploads/chorus_asset/file/9278671/jbareham_170917_2000_0124.jpg', clickAction: () => alert('custom click function') },
+    { url: 'https://cdn.vox-cdn.com/uploads/chorus_image/image/56789263/akrales_170919_1976_0104.0.jpg', caption: 'Apple TV', href: 'https://www.apple.com/' },
+    'https://cdn.vox-cdn.com/uploads/chorus_image/image/56674755/mr_pb_is_the_best.0.jpg',
+    { url: 'assets/kitties.jpg', backgroundSize: 'contain', backgroundPosition: 'center' }
+  ];
+  height: string = '400px';
+  minHeight: string;
+  arrowSize: string = '30px';
+  showArrows: boolean = true;
+  disableSwiping: boolean = false;
+  autoPlay: boolean = true;
+  autoPlayInterval: number = 3333;
+  stopAutoPlayOnSlide: boolean = true;
+  debug: boolean = false;
+  backgroundSize: string = 'cover';
+  backgroundPosition: string = 'center center';
+  backgroundRepeat: string = 'no-repeat';
+  showDots: boolean = true;
+  dotColor: string = '#FFF';
+  showCaptions: boolean = true;
+  captionColor: string = '#FFF';
+  captionBackground: string = 'rgba(0, 0, 0, .35)';
+  lazyLoad: boolean = false;
+  hideOnNoSlides: boolean = false;
+  width: string = '100%';
+
   constructor() { }
 
   ngOnInit() {
+    // adding an image url dynamically.
+   setTimeout(() => {
+     console.log('adding an image url dynamically.');
+     this.imageUrls.push('https://cdn-images-1.medium.com/max/2000/1*Nccd2ofdArlXF7v58UK94Q.jpeg');
+   }, 2000);
+   console.log(`
+                                 /   \\
+_                        )      ((   ))     (
+(@)                      /|\\      ))_((     /|\\
+|-|                     / | \\    (/\\|/\\)   / | \\                      (@)
+| | -------------------/--|-voV---\`|'/---Vov-|--\\---------------------|-|
+|-|                         '^\`   (o o)  '^\`                          | |
+| |                               \`\\Y/'                               |-|
+|-|                                                                   | |
+| |                                Hey                                |-|
+|-|                                                                   | |
+| |                                                                   |-|
+|_|___________________________________________________________________| |
+(@)              l   /\\ /         ( (       \\ /\\   l                \`\\|-|
+                l /   V           \\ \\       V   \\ l                  (@)
+                l/                _) )_          \\I
+                                  \`\\ /'
+                            \`
+   `);
   }
 
 }
