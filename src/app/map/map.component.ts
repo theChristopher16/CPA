9084400ec73f84ceab2f715a3fc5f62253f673af
Christopher Smith <christophersmithm16@gmail.com>
2019-01-27 23:55:45 -0600

    Added new map textures and buildings.

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -28,11 +28,17 @@ export class MapComponent implements OnInit {
     let green;
     let blue;
     let red;
+    let floor;
+    let orange;
+    let yellow;
 
    p.preload = () => {
       green = p.loadImage('../../assets/Map/green.jpg');
-      blue = p.loadImage('../../assets/Map/blue.jpg');
-      red = p.loadImage('../../assets/Map/red.jpg');
+      blue = p.loadImage('../../assets/Map/blue.png');
+      red = p.loadImage('../../assets/Map/red.png');
+      floor = p.loadImage('../../assets/Map/floor.png');
+      orange = p.loadImage('../../assets/Map/orange.png');
+      yellow = p.loadImage('../../assets/Map/yellow.png');
     };
 
     p.setup = () => {
@@ -41,20 +47,41 @@ export class MapComponent implements OnInit {
     };
 
     p.draw = () => {
-      p.background(175);
+      p.background(112.5 * p.cos(angle));
       p.noStroke(0);
-      p.rotateX(angle);
-      p.rotateY(angle * 0.3);
-      p.rotateZ(angle * 0.7);
+      p.rotateX(1);
+      p.rotateY(0);
+      p.rotateZ(angle);
       p.noStroke();
 
       // Normal material is for debugging
       //p.normalMaterial();
 
-      p.texture(green);
-      p.box(400, 300, 10);
+      p.texture(floor);
+      p.box(2000, 2000, 10);
 
-      angle += 0.007;
+      p.noStroke();
+      p.translate(300, -190);
+      p.texture(red);
+      p.box(150, 150, 200);
+
+      p.noStroke();
+      p.translate(-700, 190);
+      p.texture(blue);
+      p.box(150, 150, 900);
+
+
+      p.noStroke();
+      p.translate(350, 280);
+      p.texture(blue);
+      p.box(150, 150, 450);
+
+      p.noStroke();
+      p.translate(10, -800);
+      p.texture(red);
+      p.box(150, 150, 450);
+
+      angle += 0.0027;
     };
   }
 }
