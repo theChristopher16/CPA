ef25c3404b14024d8c441033dc3f9ffff5bc9042
Josh <joshuamcdowell97@gmail.com>
2019-01-31 16:48:49 -0600

    obj file

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -10,7 +10,9 @@ export class MapComponent implements OnInit {
 
   private p5;
 
-  constructor() { }
+  constructor(
+  //  private p5: p5
+) { }
 
   ngOnInit() {
     this.createCanvas();
@@ -21,18 +23,90 @@ export class MapComponent implements OnInit {
   }
 
   private sketch(p: any){
+
+    let angle = 0;
+    let green;
+    let blue;
+    let red;
+    let floor;
+    let orange;
+    let yellow;
+
+    let building;
+
+    // Josh: Added to track fps
+    let lastLoop = performance.now();
+
+   p.preload = () => {
+      /*green = p.loadImage('../../assets/Map/green.jpg');
+      blue = p.loadImage('../../assets/Map/blue.png');
+      red = p.loadImage('../../assets/Map/red.png');
+      floor = p.loadImage('../../assets/Map/floor.png');
+      orange = p.loadImage('../../assets/Map/orange.png');
+      yellow = p.loadImage('../../assets/Map/yellow.png');*/
+      floor = p.loadImage('../../assets/Map/map.png');
+      building = p.loadModel('../../assets/Map/cone.obj')
+    };
+
     p.setup = () => {
       var cnv = p.createCanvas(window.innerWidth, window.innerHeight, p.WEBGL);
       cnv.parent('myContainer');
     };
 
     p.draw = () => {
-      p.background(0);
-      p.translate(0, 0, 0)
-      p.fill(50);
-      p.box(p.width / 2, p.height / 2, 50, 50);
-  };
+      p.background(134 * p.cos(angle), 218 * p.cos(angle), 112.5 * p.cos(angle));
+      p.noStroke(0);
+      p.rotateX(1);
+      p.rotateY(0);
+      p.rotateZ(angle);
+      p.noStroke();
 
+      // Normal material is for debugging
+      //p.normalMaterial();
+      p.texture(floor);
+      p.box(4000, 4000, 10);
 
+      p.noStroke();
+      p.translate(300, -190);
+      p.texture(floor);
+      p.box(150, 150, 200);
+
+      p.noStroke();
+      p.translate(-700, 190);
+      p.texture(floor);
+      p.box(150, 150, 900);
+
+      p.noStroke();
+      p.translate(350, 280);
+      p.texture(floor);
+      p.box(150, 150, 450);
+
+      p.noStroke();
+      p.translate(10, -800);
+      p.texture(floor);
+      p.box(150, 150, 450);
+
+      
+
+      /*p.noStroke();
+      p.translate(100, 0);
+      p.texture(blue);
+      p.box(150, 150, 450);*/
+
+      // Josh: Testing out .obj model
+      p.translate(-200, 100, 140);
+      p.normalMaterial();
+      p.scale(30);
+      p.stroke(255, 255, 1); // Colors lines of shape
+      p.model(building);
+
+      angle += 0.0027;
+
+      // Josh: Calculates fps and writes it to console
+      var thisLoop = performance.now();
+      var fps = Math.round(1000 / (thisLoop - lastLoop));
+      lastLoop = thisLoop;
+      console.log(fps);
+    };
   }
 }
