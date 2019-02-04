2106ae6473de8ed076e6501c813a47b4d198620a
Josh <joshuamcdowell97@gmail.com>
2019-02-03 22:48:57 -0600

    added text above buildings

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -32,8 +32,11 @@ export class MapComponent implements OnInit {
     let orange;
     let yellow;
     let nethkin;
+    let nethkinT;
     let tolliver;
+    let tolliverT;
     let wyly;
+    let wylyT;
 
     // Josh: Added to track fps
     let lastLoop = performance.now();
@@ -50,6 +53,25 @@ export class MapComponent implements OnInit {
       nethkin = p.loadModel('../../assets/Map/nethkin.obj')
       tolliver = p.loadModel('../../assets/Map/tolliver.obj')
       wyly = p.loadModel('../../assets/Map/wyly.obj')
+    
+      // Josh: Initialize 3D text labels here
+      nethkinT = p.createGraphics(150, 150);
+      nethkinT.fill(255);
+      nethkinT.textAlign(p.CENTER);
+      nethkinT.textSize(35)
+      nethkinT.text("Nethkin", 80, 60);
+
+      tolliverT = p.createGraphics(150, 150);
+      tolliverT.fill(255);
+      tolliverT.textAlign(p.CENTER);
+      tolliverT.textSize(50)
+      tolliverT.text("Tolliver", 75, 60);
+      
+      wylyT = p.createGraphics(150, 150);
+      wylyT.fill(255);
+      wylyT.textAlign(p.CENTER);
+      wylyT.textSize(35);
+      wylyT.text("Wyly", 80, 60);
     };
 
     p.setup = () => {
@@ -58,36 +80,53 @@ export class MapComponent implements OnInit {
     };
 
     p.draw = () => {
+      // Draw background color
       p.background(134 * p.cos(angle), 218 * p.cos(angle), 112.5 * p.cos(angle));
      
+      // Move camera
       p.noStroke(0);
       p.rotateX(1);
       p.rotateY(0);
+
+      // Before rotation draw building labels
+
       p.rotateZ(angle);
       p.noStroke();
 
+      // Draw ground
       p.texture(floor);
       p.box(1152, 1152, 2);
+
+      // Note, draw text first to fix transparency bug?
 
       // Gabrielle: Wyly
       p.push();
       p.translate(125, -290, 0);
-      p.rotateX(90 * Math.PI / 180);
-      p.rotateY(15 * Math.PI/ 180);
+      p.rotateX(90 * Math.PI/180);
+      p.rotateY(15 * Math.PI/180);
       p.ambientMaterial(0);
       p.scale(18);
       p.stroke(255, 255, 1);
       p.model(wyly);
+      p.rotateX(180 * Math. PI / 180);
+      p.translate(0, -10, 0);
+      p.texture(wylyT);
+      p.plane(10);
       p.pop();
 
       // Josh: Nethkin
       p.push();
-      p.translate(510, 130, 23);
-      p.rotateX(-90 * Math.PI/180); // Fixes the 90 degree flip problem from Blender
+      p.translate(510, 130, 0);
+      p.rotateX(90 * Math.PI/180); // Fixes the 90 degree flip problem from Blender
+      p.rotateY(180 * Math.PI/180);
       p.ambientMaterial(0);
       p.scale(14);
       p.stroke(255, 255, 255); // Colors lines of shape
       p.model(nethkin);
+      p.rotateX(180 * Math. PI / 180);
+      p.translate(0, -4, 0);
+      p.texture(nethkinT);
+      p.plane(10);
       p.pop();
 
       // Josh: Tolliver
@@ -99,6 +138,11 @@ export class MapComponent implements OnInit {
       p.scale(15);
       p.stroke(255, 255, 255); // Colors lines of shape
       p.model(tolliver);
+      p.rotateX(180 * Math.PI / 180);
+      p.rotateZ(180 * Math.PI / 180);
+      p.translate(0, -1, 0);
+      p.texture(tolliverT);
+      p.plane(10);
       p.pop();
 
       angle += 0.0027;
