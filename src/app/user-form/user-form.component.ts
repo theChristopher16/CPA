2478aece54b79875001ab567e36fb5a00bce8718
Adam Banks-Beard <adam.beard96@icloud.com>
2019-04-27 21:04:29 -0500

    two way data binding for form

--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../user';
 
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
   styleUrls: ['./user-form.component.scss']
 })
-export class UserFormComponent implements OnInit {
+export class UserFormComponent{
+  
+  model = new User(null,null,null,null);
 
-  constructor() { }
+  submitted = false;
 
-  ngOnInit() {
-  }
+  onSubmit(){ this.submitted = true; }
 
+  get diagnotstic() { return JSON.stringify(this.model); }
 }
