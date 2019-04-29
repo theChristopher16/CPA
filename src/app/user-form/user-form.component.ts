566ca58e4ee825ea9f5bac56fa555895dbd8b1d4
Adam Banks-Beard <adam.beard96@icloud.com>
2019-04-29 17:05:05 -0500

    Check database to see if username taken

--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -2,21 +2,35 @@ import { Component, OnInit } from '@angular/core';
 import {User} from '../user';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 import { MatDialogRef } from '@angular/material';
+import { UserInfoService } from '../user-info.service';
+import { modelGroupProvider } from '@angular/forms/src/directives/ng_model_group';
 
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
   styleUrls: ['./user-form.component.scss']
 })
-export class UserFormComponent{
+export class UserFormComponent implements OnInit{
   
-  constructor(public dialogRef: MatDialogRef<SidebarComponent>){}
+  constructor(private user: UserInfoService, public dialogRef: MatDialogRef<SidebarComponent>){
+
+  }
 
   model = new User(null,null,null,null);
-
   submitted = false;
-
   userNameTaken = false;
+  users$: object;
+
+  ngOnInit(): void {
+    
+    // Subscribe to service to get scores from database
+    this.user.getUserInfo().subscribe(
+      user => {
+        this.users$ = user;
+        console.log(this.users$);
+        });
+
+  }
 
   onSubmit(){ this.submitted = true; }
 
@@ -32,8 +46,22 @@ export class UserFormComponent{
   //makes sure username is not taken
   verifyForm(){
 
-    //TODO: check if username is taken
+    //Go thru users from database
+    //If match -> name is taken; break
+    //if no match -> name not taken
+    let i = 0;
+    for(let person in this.users$){
+      if(this.users$[i]["Name"] === this.model.userName){
+        this.userNameTaken = true;
+        break;
+      }
+      else{
+        this.userNameTaken = false;
+      }
+      i++;
+    }
 
+    //closes dialog and submits form if name available
     if(!this.userNameTaken){
       this.closeDialog();
       this.submitted = true;
@@ -42,5 +70,4 @@ export class UserFormComponent{
 
   }
 
-  get diagnotstic() { return JSON.stringify(this.model); }
 }
