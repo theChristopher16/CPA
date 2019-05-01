f7e174cc3b56be20bea9311091824d28c0f9f80c
Christopher Smith <cms095@latech.edu>
2019-05-01 12:44:18 -0500

    more user install changes.

--- backend/newUser/CreateUser/createUserScript.sh
+++ backend/newUser/CreateUser/createUserScript.sh
@@ -7,10 +7,10 @@ echo $pass > password.txt
 cat *.txt > user.txt
 rm username.txt
 rm password.txt
-pssh -i -h .hosts.txt StrictHostKeyChecking=no "scp winadmin@10.16.17.254:/home/winadmin/Storage/newUser/CreateUser/userPassword.py /home/winadmin"
-pssh -i -h .hosts.txt StrictHostKeyChecking=no "scp winadmin@10.16.17.254:/home/winadmin/Storage/newUser/CreateUser/user.txt /home/winadmin"
-pssh -i -h .hosts.txt StrictHostKeyChecking=no "scp -r winadmin@10.16.17.254:/home/winadmin/Storage/newUser/descriptions /home/winadmin"
-pssh -i -h .hosts.txt StrictHostKeyChecking=no "scp winadmin@10.16.17.254:/home/winadmin/Storage/newUser/CreateUser/installHost.sh /home/winadmin"
+pssh -i -h .hosts.txt StrictHostKeyChecking=no "scp winadmin@10.16.17.254:/home/winadmin/Development/CPA/backend/newUser/CreateUser/userPassword.py /home/winadmin"
+pssh -i -h .hosts.txt StrictHostKeyChecking=no "scp winadmin@10.16.17.254:/home/winadmin/Development/CPA/backend/newUser/CreateUser/user.txt /home/winadmin"
+pssh -i -h .hosts.txt StrictHostKeyChecking=no "scp -r winadmin@10.16.17.254:/home/winadmin/Development/CPA/backend/newUser/descriptions /home/winadmin"
+pssh -i -h .hosts.txt StrictHostKeyChecking=no "scp winadmin@10.16.17.254:/home/winadmin/Development/CPA/backend/newUser/CreateUser/installHost.sh /home/winadmin"
 pssh -i -h .hosts.txt StrictHostKeyChecking=no ./installHost.sh
 pssh -i -h .hosts.txt StrictHostKeyChecking=no rm user.txt
 pssh -i -h .hosts.txt StrictHostKeyChecking=no rm userPassword.py
@@ -19,3 +19,5 @@ pssh -i -h .hosts.txt StrictHostKeyChecking=no rm -r descriptions
 pssh -i -h .hosts.txt StrictHostKeyChecking=no rm -r __pycache__
 rm -r __pycache__
 rm user.txt
+curlCmd = "url -F 'Name='$usn'' -F 'Key=SSBsb3ZlIHRpZGRpZXM' -X POST http://ec2-34-219-116-164.us-west-2.compute.amazonaws.com/addUser"
+eval curlCMD
