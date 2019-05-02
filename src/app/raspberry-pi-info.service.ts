f02b73705cf5ab500292287c4bf9e073a105ce7a
Adam Banks-Beard <adam.beard96@icloud.com>
2019-05-01 19:53:44 -0500

    Made changes to services to pull from webserver

--- src/app/raspberry-pi-info.service.ts
+++ src/app/raspberry-pi-info.service.ts
@@ -11,6 +11,7 @@ export class RaspberryPiInfoService {
 
   getRaspberryPiNetwork(){
     //return this.http.get('http://127.0.0.1:8080/networkStatus')
-    return this.http.get('http://ec2-34-219-116-164.us-west-2.compute.amazonaws.com/networkStatus')
+    //return this.http.get('http://ec2-34-219-116-164.us-west-2.compute.amazonaws.com/networkStatus')
+    return this.http.get('http://winnet.duckdns.org:8080/networkStatus');
   }
 }
