b6cef38bd1a8538513521d16fc94a585fd91ec83
Christopher Smith <cms095@latech.edu>
2019-05-06 10:28:37 -0500

    Slideshow fix.

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -18,13 +18,13 @@ export class InfoComponent implements OnInit {
   constructor(private router: Router, private tabscroller: TabScrollerService) {
   this.items = [
     { name: '../../assets/Info/Images/img1.png' },
+    { name: '../../assets/Info/Images/img2.png' },
     { name: '../../assets/Info/Images/img3.png' },
     { name: '../../assets/Info/Images/img1.png' },
+    { name: '../../assets/Info/Images/img2.png' },
     { name: '../../assets/Info/Images/img3.png' },
     { name: '../../assets/Info/Images/img1.png' },
-    { name: '../../assets/Info/Images/img3.png' },
-    { name: '../../assets/Info/Images/img1.png' },
-    { name: '../../assets/Info/Images/img3.png' }
+    { name: '../../assets/Info/Images/img2.png' }
   ];
   }
 
